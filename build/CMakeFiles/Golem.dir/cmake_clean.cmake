5f3f4a22f3f49e80a57c4102568a90edae9b128b
Ian Bowman <ianbow111@gmail.com>
2023-03-05 13:18:25 -0500

    Fixed a semicolon.

---
+++ build/CMakeFiles/Golem.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Golem.dir/golem.c.o"
+  "CMakeFiles/Golem.dir/golem.c.o.d"
+  "CMakeFiles/Golem.dir/patcher.c.o"
+  "CMakeFiles/Golem.dir/patcher.c.o.d"
+  "Golem"
+  "Golem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Golem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
